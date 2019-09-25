f0a45bab063f9969509363f9194be0790113efaf
Piotr (Peter) Slatala <psla-github@sepio.pl>
2019-09-24 18:55:05 -0700

    Fetch all of the required tiles (and cache them on disk)

---
+++ PicturesToGpx/Fetcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace PicturesToGpx
+{
+    internal class Fetcher
+    {
+        internal Fetcher(string cacheDir = @"G:\tmp\tile-cache")
+        {
+            this.cacheDir = cacheDir;
+        }
+
+        internal static readonly string BlackTile = "BLACKTILE";
+
+        private readonly string cacheDir;
+
+        private static readonly Regex AsciiRegex = new Regex("[^a-zA-Z0-9]", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        internal Bitmap Fetch(string url)
+        {
+            if (url == BlackTile)
+            {
+                return new Bitmap(256, 256);
+            }
+
+            var normalizedFilename = AsciiRegex.Replace(url, "-");
+            Console.WriteLine("Fetching {0} to {1}", url, normalizedFilename);
+            var targetPath = Path.Combine(cacheDir, normalizedFilename);
+
+            if (File.Exists(targetPath))
+            {
+                return new Bitmap(targetPath);
+            }
+
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                wc.DownloadFile(url, targetPath);
+            }
+
+            return new Bitmap(targetPath);
+        }
+    }
+}
--- PicturesToGpx/Tiler.cs
+++ PicturesToGpx/Tiler.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Drawing;
+using System.Globalization;
 
 namespace PicturesToGpx
 {
     internal class Tiler
     {
+        private const int TileWidthHeight = 256;
 
         // lyrs=h/m/
         //h = roads only
@@ -14,8 +17,8 @@ namespace PicturesToGpx
         //t = terrain only
         //y = hybrid
         private static readonly string GoogleMapsUrl = "http://mt1.google.com/vt/lyrs=m&x={0}&y={1}&z={2}";
-        private static readonly string BlackTile = "BLACKTILE";
 
+        private static readonly Fetcher fetcher = new Fetcher();
 
         internal static void RenderEmptyMap(BoundingBox boundingBox, string outgoingPicturePath, int widthPx, int heightPx)
         {
@@ -25,18 +28,25 @@ namespace PicturesToGpx
             var midX = LocationUtils.GetX(zoomLevel, boundingBox.MiddleLatitude);
             var midY = LocationUtils.GetX(zoomLevel, boundingBox.MiddleLongitude);
 
-            var noOfTilesPerWidth = (widthPx - 1) / 256 + 1;
-            var noOfTilesPerHeight = (heightPx - 1) / 256 + 1;
+            var noOfTilesPerWidth = (widthPx - 1) / TileWidthHeight + 1;
+            var noOfTilesPerHeight = (heightPx - 1) / TileWidthHeight + 1;
             for (int y = midY - noOfTilesPerHeight / 2; y <= midY + noOfTilesPerHeight / 2; y++)
             {
                 for (int x = midX - noOfTilesPerWidth / 2; x <= midX + noOfTilesPerWidth / 2; x++)
                 {
                     // or too far right. TODO
-                    if (y < 0 || x < 0)
+                    if (y < 0 || x < 0 || x >= Math.Pow(2, zoomLevel) || y >= Math.Pow(2, zoomLevel))
                     {
-                        Console.WriteLine(BlackTile);
+                        Console.WriteLine(Fetcher.BlackTile);
                     }
-                    Console.WriteLine(GoogleMapsUrl, x, y, zoomLevel);
+                    else
+                    {
+                        Console.WriteLine(GoogleMapsUrl, x, y, zoomLevel);
+                        using (Bitmap b = fetcher.Fetch(string.Format(CultureInfo.InvariantCulture, GoogleMapsUrl, x, y, zoomLevel)))
+                        {
+
+                        }
+                    }
                 }
             }
         }
