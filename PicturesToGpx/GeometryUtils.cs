151a81abfe1639bb26d7d13c5f336c29832f8436
Piotr (Peter) Slatala <psla-github@sepio.pl>
2019-09-26 18:48:36 -0700

    Implement Chaikin

--- PicturesToGpx/GeometryUtils.cs
+++ PicturesToGpx/GeometryUtils.cs
@@ -2,7 +2,7 @@
 
 namespace PicturesToGpx
 {
-    internal class GeometryUtils
+    public static class GeometryUtils
     {
         // TODO: List of (x,y) in pixels instead of positions!
         public static List<Position> SkipTooClose(List<Position> input)
@@ -10,9 +10,43 @@ namespace PicturesToGpx
             return input;
         }
 
-        public static List<Position> SmoothLine(List<Position> input)
+        public class ChaikinSettings
         {
-            return input;
+            public double WhereToRound { get; set; } = 0.9;
+        }
+
+        public static List<Position> SmoothLineChaikin(this List<Position> input, ChaikinSettings settings)
+        {
+            if (input.Count < 2)
+            {
+                return input;
+            }
+
+            var output = input;
+            int iterationCount = 0;
+            do
+            {
+                input = output;
+                output = new List<Position>();
+                output.Add(input[0]);
+                for (int i = 0; i < input.Count - 2; i++)
+                {
+                    output.Add(
+                        new Position(
+                        input[i].Time.AddSeconds((input[i + 1].Time - input[i].Time).TotalSeconds * settings.WhereToRound),
+                        (input[i + 1].Latitude - input[i].Latitude) * settings.WhereToRound + input[i].Latitude,
+                        (input[i + 1].Longitude - input[i].Longitude) * settings.WhereToRound + input[i].Longitude));
+
+                    double whereToRoundSecond = (1 - settings.WhereToRound);
+                    output.Add(new Position(
+                        input[i + 1].Time.AddSeconds((input[i + 2].Time - input[i + 1].Time).TotalSeconds * (1 - settings.WhereToRound)),
+                        (input[i + 2].Latitude - input[i + 1].Latitude) * (1 - settings.WhereToRound) + input[i + 1].Latitude,
+                        (input[i + 2].Longitude - input[i + 1].Longitude) * whereToRoundSecond + input[i + 1].Longitude));
+                }
+                output.Add(input[input.Count - 1]);
+                iterationCount++;
+            } while (output.Count != input.Count && iterationCount < 10);
+            return output;
         }
     }
 }
--- PicturesToGpx/Program.cs
+++ PicturesToGpx/Program.cs
@@ -148,7 +148,7 @@ namespace PicturesToGpx
 
         private static void CreateMapFromPoints(string pointPath, string outgoingPicturePath)
         {
-            var points = JsonConvert.DeserializeObject<List<Position>>(File.ReadAllText(pointPath)).Skip(300).Take(150).ToList();
+            var points = JsonConvert.DeserializeObject<List<Position>>(File.ReadAllText(pointPath)).Skip(300).Take(150).ToList().SmoothLineChaikin(new GeometryUtils.ChaikinSettings());
 
             points = points.Select(LocationUtils.ToMercator).ToList();
             var boundingBox = LocationUtils.GetBoundingBox(points);
